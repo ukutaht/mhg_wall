39e4006d35afffb3411a0424b262598a55534e2a
Uku Täht <uku.taht@gmail.com>
2013-03-11 20:49:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MhgWall::Application.config.secret_token = 'b1a42ae0cbb562c5e60a9904e11c94645cc5ddb9a60b1ded069b5ed4d6e4066041b7693a4f6951b3a639677a5b3776bb333df1bb9420a0690246d5a5d92dd978'
